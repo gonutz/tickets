d65d3081183f5b3c0858cd8f904411c2a9635f0f
Lars <lars.scheme@gmail.com>
2019-01-23 18:13:41 +0100

    Add batch file to show closed tickets

--- build_ticket_apps.bat
+++ build_ticket_apps.bat
@@ -1,7 +1,9 @@
-go get github.com/gonutz/wui
-if errorlevel 1 pause
 set GOARCH=386
+
 go build -ldflags="-s -w" -o "Create new Ticket.exe" new_ticket.go
 if errorlevel 1 pause
+
 go build -ldflags="-s -w" -o "View Tickets.exe" view_tickets.go
 if errorlevel 1 pause
+
+echo git log --diff-filter=D --summary ./*.txt ^& pause>"Show Closed Tickets.bat"
